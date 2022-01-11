e77034b5acfc93773d70e4744a5f074a9c5c8258
Ganesh <ganesh.birajdar.1930@gmail.com>
2022-01-11 15:57:36 +0530

    done

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,30 +4,22 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo">
+      {/* <div className="logo">
         <Image src="/landing.png" width={105} height={100} />
-      </div>
+      </div> */}
       <Link href="/">
         <a>Home</a>
       </Link>
       <Link href="/about">About</Link>
       <Link href="/ninjas">
-        <a>BLog Listing</a>
+        <a>Blog Listing</a>
       </Link>
-      <Link href="/about">
-        <a>Technologies</a>
-      </Link>
-      <Link href="/about">
-        <a>Community</a>
-      </Link>
+
       <Link href="/about">
         <a>Team</a>
       </Link>
       <Link href="/about">
         <a>Get in touch</a>
-      </Link>
-      <Link href="/about">
-        <a>Be a Member</a>
       </Link>
     </nav>
   );
